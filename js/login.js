90bc4c5b0b85ffd7f5b93c4e8335e7d1fd99deed
matifandy <matifandy@gmail.com>
2020-08-13 11:36:44 -0300

    login

--- js/login.js
+++ js/login.js
@@ -4,3 +4,35 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function validation(){
+    var email = document.getElementById('inputEmail').value;
+    var password = document.getElementById('inputPassword').value;
+    var error_message = document.getElementById('error_message');
+    var text;
+
+    error_message.style.padding = "10px";
+
+    if(email === ''){
+      text = "Escriba un email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(password === ''){
+      text = "Escriba una contraseña";
+      error_message.innerHTML = text;
+      return false;
+    }
+    window.location="index.html"
+    return true;
+}
+
+// ------------------------
+
+const form = document.getElementById('myform');
+
+form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      validation();
+});
