2426ea45bdf0cfc98844a69a9ab098671e4f7b01
matifandy <matifandy@gmail.com>
2020-10-29 14:48:12 -0300

    navbar resposive

--- js/products.js
+++ js/products.js
@@ -28,20 +28,20 @@ function showProductsList() {
     ) {
       htmlContentToAppend +=
         `
-        <div class="col-6">
+        <div class="col-md-6">
         <a href="product-info.html?` +
         product.name +
         `" class="list-group-item list-group-item-action">
         <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-6">
+            <div class="">
+                <div class="col-md-12">
                     <img src="` +
         product.imgSrc +
         `" alt="` +
         product.desc +
         `" class="img-thumbnail">
                 </div>
-                <div class="col">
+                <div class="">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">` +
         product.name +
